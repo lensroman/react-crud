d112c6bd4f6e22103d8ad88197a659a8ab43ce20
Roman <lensroman@bk.ru>
2021-12-13 12:09:20 +0300

    Markup start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import SidePanel from "./components/SidePanel/SidePanel";
+import Markup from "./containers/Markup/Markup";
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
           <SidePanel />
+          <Markup />
       </Layout>
     </div>
   );
