407a7aed59e4968cd3231b5335ccd3897632fd02
Roman <lensroman@bk.ru>
2021-12-13 12:04:45 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+
 import Layout from "./hoc/Layout/Layout";
 import SidePanel from "./components/SidePanel/SidePanel";
+import Canvas from './components/Canvas/Canvas';
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
           <SidePanel />
+          <Canvas />
       </Layout>
     </div>
   );
