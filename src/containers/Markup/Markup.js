7d8bd1d10bfc939d0d4239daa9024934073773bb
Roman <lensroman@bk.ru>
2021-12-13 12:42:36 +0300

    Markup start

--- src/containers/Markup/Markup.js
+++ src/containers/Markup/Markup.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, {useEffect} from "react";
 
-import image from '../../assets/image.jpeg';
-// import BaseImage from "../../components/BaseImage/BaseImage";
-// import {Stage} from "react-konva";
+import { Stage, Layer, Rect, Circle } from 'react-konva';
 
 const Markup = props => {
+
 
     return (
         <div>
@@ -14,9 +13,13 @@ const Markup = props => {
                 <button>Следующее изображения</button>
                 <button>Сохранить</button>
             </div>
-            <img src={image} alt="markup" />
-            Canvas
-            Table
+
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    <Rect width={50} height={50} fill="red" />
+                    <Circle x={200} y={200} stroke="black" radius={50} />
+                </Layer>
+            </Stage>
         </div>
     )
 };
