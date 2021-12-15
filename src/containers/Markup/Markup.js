37ad7b473cf4e90ec180f57637c4ca976925860c
Roman <lensroman@bk.ru>
2021-12-15 12:07:51 +0300

    Markup. onmousedown, onmousemove, onmouseup events implemented.

--- src/containers/Markup/Markup.js
+++ src/containers/Markup/Markup.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import image from '../../assets/image.png';
 import MarkupHeader from "../../components/markupHeader/MarkupHeader";
@@ -7,38 +7,75 @@ import useImage from "use-image";
 
 const Markup = props => {
 
-    const [linesClick, setLinesClick] = useState([]);
+    // regions = [
+    //     {
+    //         id: 123,
+    //         categoryId: 'building',
+    //         color: 'randomColor',
+    //         segmentation: points,
+    //     },
+    //     {
+    //         id: 123,
+    //         categoryId: 'building',
+    //         color: 'randomColor',
+    //         segmentation: points,
+    //     }
+    // ]
 
-    const [imgDOM] = useImage(image);
+    const [regions, setRegions] =useState([])
+    const [points, setPoints] = useState([])
+    const [firstLine, setFirstLine] = useState(true)
+    const [startDraw, setStartDraw] = useState(false)
+
+    const [imgDOM] = useImage(image)
 
     const getRelativePointerPosition = (node) => {
-        const transform = node.getAbsoluteTransform().copy();
-
-        transform.invert();
-
-        const pos = node.getStage().getPointerPosition();
-
-        return transform.point(pos);
+        const transform = node.getAbsoluteTransform().copy()
+        transform.invert()
+        const pos = node.getStage().getPointerPosition()
+        return transform.point(pos)
     };
 
-    const canvasClickHandler = (event) => {
-        let newLines = [...linesClick];
+    const canvasMouseDownHandler = (event) => {
+        console.log('outside', firstLine)
+        if (firstLine) {
+            console.log('inside', firstLine)
+            let newPoints = []
+            const point = getRelativePointerPosition(event.target.getStage())
+            newPoints.push(point.x, point.y)
+            setPoints(newPoints)
+            setStartDraw(true)
+        }
+    }
 
+    const canvasMouseMoveHandler = (event) => {
+        if (startDraw) {
+            const point = getRelativePointerPosition(event.target.getStage())
+            let newPoints = []
+            newPoints = [points[0], points[1], point.x, point.y]
+            setPoints(newPoints)
+        }
+    }
+
+    const canvasMouseUpHandler = event => {
+        setStartDraw(false)
+        let newPoints = [...points]
         const point = getRelativePointerPosition(event.target.getStage());
-
-        newLines.push(point.x, point.y);
-
-        setLinesClick(newLines);
-    };
-
+        newPoints.push(point.x, point.y)
+        setPoints(newPoints)
+        setFirstLine(false)
+    }
 
     return (
         <div>
             <MarkupHeader />
             <Canvas
                 image={imgDOM}
-                click={(event) => canvasClickHandler(event)}
-                linesMarkup={linesClick}
+                mouseDown={(event) => canvasMouseDownHandler(event)}
+                mouseMove={(event) => canvasMouseMoveHandler(event)}
+                mouseUp={(event) => canvasMouseUpHandler(event)}
+                pointsMarkup={points}
+                startDraw={startDraw}
             />
         </div>
     )
