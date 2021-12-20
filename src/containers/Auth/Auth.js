c768b926ea1ce74855b120a70731e9f95fc69753
Roman <lensroman@bk.ru>
2021-12-20 10:02:54 +0300

    Re-used components Button and Input added. Auth container created.

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Input from "../../components/UI/Input/Input";
+
+const Auth = props => {
+    // state for organization data for request
+        // object with tight structure like backend has
+
+    // re-used input component
+        // placeholders, onChange methods, right types
+
+    // validation
+        // email validation, hide password with dots
+
+    return (
+        <div>
+            <Input />
+        </div>
+    )
+}
+
+export default Auth
